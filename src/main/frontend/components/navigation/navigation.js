c975969670cf7493b22b9dd640d84e08e1f9da21
Petru Csokany <petru.csokany@gmail.com>
2022-03-17 21:38:02 +0200

    Added node modules to gitignore

--- src/main/frontend/components/navigation/navigation.js
+++ src/main/frontend/components/navigation/navigation.js
@@ -4,49 +4,49 @@ import "./navigation.css";
 
 class Navigation extends React.Component {
 
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	componentDidMount() {
-	}
+    componentDidMount() {
+    }
 
-	render() {
-		const { t } = this.props;
-		return (
-			<div className="navigation">
-
-				<nav class="navbar navbar-dark bg-dark">
-					<ul class="nav justify-content-end">
-						
-						<li className="nav-item">
-							<NavLink className="nav-link" to="/">
-								Google Maps Location
-							</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink className="nav-link" to="/login">
-								Login
-							</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink className="nav-link" to="/registration">
-								Registration
-							</NavLink>
-						</li>
-
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li><button onClick={sayHello} type="button" class="btn btn-primary btn-small btn-nav">Log Out</button></li>
-					</ul>
-				</nav>
-			</div>
-		);
-	}
+    render() {
+        const {t} = this.props;
+        return (
+                <div className="navigation">
+                
+                    <nav class="navbar navbar-dark bg-dark">
+                        <ul class="nav justify-content-end">
+                
+                            <li className="nav-item">
+                            <NavLink className="nav-link" to="/">
+                                Google Maps Location
+                            </NavLink>
+                            </li>
+                            <li className="nav-item">
+                            <NavLink className="nav-link" to="/login">
+                                Login
+                            </NavLink>
+                            </li>
+                            <li className="nav-item">
+                            <NavLink className="nav-link" to="/registration">
+                                Registration
+                            </NavLink>
+                            </li>
+                
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><button onClick={sayHello} type="button" class="btn btn-primary btn-small btn-nav">Log Out</button></li>
+                        </ul>
+                    </nav>
+                </div>
+                );
+    }
 }
 
 function sayHello() {
-	alert('You clicked me!');
+    alert('You clicked me!');
 }
 
 export default Navigation;
